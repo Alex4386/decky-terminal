f730bed579e7eab5e128d15ca1414c660806aa48
Alex4386 <psh010209@gmail.com>
2023-08-23 01:53:44 +0900

    chore: stub settings page for #4

--- src/pages/settings/SettingsPage.tsx
+++ src/pages/settings/SettingsPage.tsx
@@ -1,13 +1,33 @@
 import {
-    Focusable,
+    Dropdown,
+    Focusable, staticClasses,
   } from "decky-frontend-lib";
   import { VFC } from "react";
   
   
   const SettingsPage: VFC = () => {
     return (
-        <Focusable>
-            <p>This page is STUB!</p>
+        <Focusable style={{ display: 'flex', gap: '.5rem', flexDirection: 'column'}}>
+            <Focusable
+                style={{ display: 'flex', justifyContent: 'space-between', gap: '1rem' }}>
+                <div>
+                    <div className={staticClasses.Text}>Default Shell</div>
+                    <div className={staticClasses.Label}>Change the default shell of the terminal</div>
+                </div>
+                <div style={{ minWidth: '150px' }}>
+                    <Dropdown
+                        disabled={false}
+                        selectedOption={"/bin/bash"}
+                        onChange={(e) => {console.log(e.data)}}
+                        rgOptions={[{
+                            label: "/bin/bash",
+                            data: "/bin/bash"
+                        }, {
+                            label: "/bin/zsh",
+                            data: "/bin/zsh"
+                        }]} />
+                </div>
+            </Focusable>
         </Focusable>
     );
   };
