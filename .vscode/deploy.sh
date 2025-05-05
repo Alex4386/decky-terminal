742a8b3c30e476346edcaeb2518e8a7efa1ae97b
Alex4386 <psh010209@gmail.com>
2025-05-06 01:15:20 +0900

    ci: automatic deployment

--- .vscode/deploy.sh
+++ .vscode/deploy.sh
@@ -15,3 +15,4 @@ sudo chown -R $USER $(pwd)/out
 
 # Custom build routine epilogue
 scp "$(pwd)/out/Decky Terminal.zip" deck@deploy-deck:$TARGET_PATH
+ssh -t deck@deploy-deck "sudo cp "$TARGET_PATH" ./homebrew/plugins/decky-terminal.zip && cd ./homebrew/plugins/ && sudo rm -rf './Decky Terminal' && sudo unzip decky-terminal.zip && sudo rm -rf decky-terminal.zip && sudo systemctl restart plugin_loader.service"
